315dda4e37c5df528d23e646dfeb0a5bd727c567
i-himanshu <himanshu.kumar5403@gmail.com>
2024-01-21 18:05:15 +0530

    Bug Fixes

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(5500, ()=> {
+app.listen(8080, ()=> {
     console.log ("server is listening to port 8080");
 });
 
