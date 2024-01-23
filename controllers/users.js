f0180a441011c34c896193a141af960fe35947d3
rishugren03 <rishugren03@gmail.com>
2024-01-24 02:54:49 +0530

    ccommit

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ module.exports.signup = async(req, res) => {
          if(err) {
              return next(err);
          }
-         req.flash("success", "Welcome to wanderlust");
+         req.flash("success", "Welcome to NourishNet");
          res.redirect("/listings");
      });
     } catch(e) {
@@ -29,7 +29,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login =  async (req, res) => {
-    req.flash("success", "Welcome back to wanderlust!");
+    req.flash("success", "Welcome back to NourishNet!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
